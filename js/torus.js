51a70f21185ced0189f10dbae7db66c320ea001f
hjhjaueon97@naver.com <hjhjaueon97@naver.com>
2022-06-17 01:44:09 +0900

    update gui

--- js/torus.js
+++ js/torus.js
@@ -1,5 +1,3 @@
-// const h_scr = window.innerWidth;
-// const v_scr = window.innerHeight;
 const h_scr = 450;
 const v_scr = 300;
 const scene = new THREE.Scene();
@@ -15,36 +13,53 @@ scene.add(light1);
 const light2 = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light2);
 
+const $radius_out = document.querySelector("#radius_out");
+const $tube_out = document.querySelector("#tube_out");
+const $radialSeg_out = document.querySelector("#radialSeg_out");
+const $tubularSeg_out = document.querySelector("#tubularSeg_out");
+const $arc_out = document.querySelector("#arc_out");
+
 const $range_radius = document.querySelector("#range_radius");
-const $output = document.querySelector("#output");
-$output.innerHTML = $range_radius.value;
-$range_radius.oninput = () => {
-  $output.innerHTML = $range_radius.value;
+const $range_tube = document.querySelector("#range_tube");
+const $range_radialSeg = document.querySelector("#range_radialSeg");
+const $range_tubularSeg = document.querySelector("#range_tubularSeg");
+const $range_arc = document.querySelector("#range_arc");
+
+$radius_out.innerText = $range_radius.value;
+$tube_out.innerText = $range_tube.value;
+$radialSeg_out.innerText = $range_radialSeg.value;
+$tubularSeg_out.innerText = $range_tubularSeg.value;
+$arc_out.innerText = $range_arc.value;
+
+$range_radius.oninput = onChangeEvent;
+$range_tube.oninput = onChangeEvent;
+$range_radialSeg.oninput = onChangeEvent;
+$range_tubularSeg.oninput = onChangeEvent;
+$range_arc.oninput = onChangeEvent;
+
+function onChangeEvent() {
+  $radius_out.innerText = $range_radius.value;
+  $tube_out.innerText = $range_tube.value;
+  $radialSeg_out.innerText = $range_radialSeg.value;
+  $tubularSeg_out.innerText = $range_tubularSeg.value;
+  $arc_out.innerText = $range_arc.value;
   scene.remove(scene.children[2]);
   const geo = new THREE.TorusGeometry(
     parseFloat($range_radius.value),
-    0.2,
-    100,
-    100
+    parseFloat($range_tube.value),
+    parseInt($range_radialSeg.value),
+    parseInt($range_tubularSeg.value),
+    parseFloat($range_arc.value)
   );
   r(geo);
-  // geometry.parameters.radius = parseFloat($range_radius.value);
-  // renderer.render(scene, camera);
-  console.log($range_radius.value);
-};
-
-// const geometry = new THREE.TorusGeometry(
-//   parseFloat($range_radius.value),
-//   0.2,
-//   100,
-//   100
-// );
+}
 const material = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   shininess: 90.0,
 });
 
-r();
+r(new THREE.TorusGeometry(0.6, 0.2, 100, 100));
+
 function r(geo) {
   const torus = new THREE.Mesh(geo, material);
   scene.add(torus);
