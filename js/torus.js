f8124f8e76ad96f31edf7bab0238a855e30bb163
hjhjaueon97@naver.com <hjhjaueon97@naver.com>
2022-06-17 01:02:46 +0900

    update gui

--- js/torus.js
+++ js/torus.js
@@ -15,18 +15,45 @@ scene.add(light1);
 const light2 = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light2);
 
-const geometry = new THREE.TorusGeometry(0.6, 0.1, 20, 120);
+const $range_radius = document.querySelector("#range_radius");
+const $output = document.querySelector("#output");
+$output.innerHTML = $range_radius.value;
+$range_radius.oninput = () => {
+  $output.innerHTML = $range_radius.value;
+  scene.remove(scene.children[2]);
+  const geo = new THREE.TorusGeometry(
+    parseFloat($range_radius.value),
+    0.2,
+    100,
+    100
+  );
+  r(geo);
+  // geometry.parameters.radius = parseFloat($range_radius.value);
+  // renderer.render(scene, camera);
+  console.log($range_radius.value);
+};
+
+// const geometry = new THREE.TorusGeometry(
+//   parseFloat($range_radius.value),
+//   0.2,
+//   100,
+//   100
+// );
 const material = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   shininess: 90.0,
 });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
 
-const animate = function () {
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
-animate();
+r();
+function r(geo) {
+  const torus = new THREE.Mesh(geo, material);
+  scene.add(torus);
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  };
+  animate();
+}
