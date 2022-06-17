08a1b038d56106e9f681171998ff85e4d9665da2
hjhjaueon97@naver.com <hjhjaueon97@naver.com>
2022-06-17 18:28:47 +0900

    update

--- js/donut.js
+++ js/donut.js
@@ -9,20 +9,11 @@ renderer3.setSize(h_scr3, v_scr3);
 
 const light5 = new THREE.AmbientLight(0xffffff, 1);
 scene3.add(light5);
-// const donutGeo = new THREE.TorusGeometry(1.5, 0.6, 100, 120);
-// const donutTex = new THREE.TextureLoader().load(
-//   "https://media.istockphoto.com/vectors/chocolate-flow-on-the-pink-seamless-donut-texture-vector-id821527352"
-// );
-// const donutMat = new THREE.MeshLambertMaterial({
-//   map: donutTex,
-// });
-// const donut = new THREE.Mesh(donutGeo, donutMat);
-// scene3.add(donut);
 
 const $donut_tube = document.querySelector("#donut_tube");
 $donut_tube.oninput = () => {
   scene3.remove(scene3.children[1]);
-  const geo = new THREE.TorusGeometry(
+  const donutGeo = new THREE.TorusGeometry(
     1.5,
     parseFloat($donut_tube.value),
     100,
@@ -34,7 +25,7 @@ $donut_tube.oninput = () => {
   const mat = new THREE.MeshLambertMaterial({
     map: donutTex,
   });
-  r(geo, mat);
+  r(donutGeo, mat);
 };
 
 const donutTex = new THREE.TextureLoader().load(
@@ -43,9 +34,9 @@ const donutTex = new THREE.TextureLoader().load(
 const mat = new THREE.MeshLambertMaterial({
   map: donutTex,
 });
-r(new THREE.TorusGeometry(1.5, 0.7, 100, 120), mat);
+renderDonut(new THREE.TorusGeometry(1.5, 0.7, 100, 120), mat);
 
-function r(geo, mat) {
+function renderDonut(geo, mat) {
   const donut = new THREE.Mesh(geo, mat);
   scene3.add(donut);
 
